19a3a9941f28721d55883785e58adda9f5e71e15
leoyoung <yangtao@23mofang.com>
2020-10-19 17:22:49 +0800

    feat: add vscode.postMessage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+declare const acquireVsCodeApi: () => {
+  getState: () => any;
+  setState: (any) => void;
+  postMessage: (any) => void;
+};
+
+const vscode = acquireVsCodeApi();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'VSCode Webview Angular';
+
+  /**
+   * onTitleClick
+   */
+  public onTitleClick() {
+    vscode.postMessage({
+      command: 'alert',
+      text: this.title,
+    });
+  }
 }
